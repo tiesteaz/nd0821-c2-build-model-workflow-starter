f8ab662d338943d9a1bc469b5acc86f708199b4f
Andrey Baranov <andrey.baranov@acronis.com>
2022-05-22 12:12:04 -0400

    fixing basic_cleansing task based on the failed test

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,9 @@ def go(args):
     logger.info("'last_review' - fix data type...");
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Data cleansing done");
 
     # saving result in the local file
