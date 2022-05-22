5455a5934bd16a5a2a37473761145d9e24a75004
Andrey Baranov <tiesteaz@gmail.com>
2022-05-22 12:15:53 -0400

    makeup change: added comment to the basic_cleaning/run.py regarding

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -35,6 +35,7 @@ def go(args):
     logger.info("'last_review' - fix data type...");
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    # remove data having wrong geo location
     idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
     df = df[idx].copy()
 
